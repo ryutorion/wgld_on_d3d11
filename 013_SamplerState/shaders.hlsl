aa1fa0905a6945f026b94eb0812e4007897b4815
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2019-04-16 22:41:59 +0900

    サンプラーステートを複数確認できる画面を実装

---
+++ 013_SamplerState/shaders.hlsl
@@ -0,0 +1,54 @@
+cbuffer CBScene
+{
+	float4x4 VP;
+};
+
+cbuffer CBLight
+{
+	float4 light_pos;
+	float4 ambient_color;
+	float4 eye_dir;
+};
+
+cbuffer CBModelVS
+{
+	float4x4 W;
+};
+
+cbuffer CBModelPS
+{
+	float4x4 IW;
+};
+
+Texture2D tex1;
+Texture2D tex2;
+SamplerState ss;
+
+struct VSInput
+{
+	float4 position : POSITION;
+	float4 normal : NORMAL;
+	float4 color : COLOR;
+	float2 uv : TEXCOORD;
+};
+
+struct PSInput
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+PSInput VS(VSInput input)
+{
+	PSInput output;
+
+	output.position = mul(mul(input.position, W), VP);
+	output.uv = input.uv;
+
+	return output;
+}
+
+float4 PS(PSInput input) : SV_TARGET
+{
+	return tex1.Sample(ss, input.uv) * tex2.Sample(ss, input.uv);
+}
